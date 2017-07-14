db37f47a32a9ead407513f7d1897552505b58181
Ali <escrime.ali@virginmedia.com>
2017-07-14 23:36:58 +0100

    Started making scripts for shooting

---
+++ 6Shooter/Assets/Shoot.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+private var Laser : GameObject;
+
+
+function Start () {
+	var collider : CapsuleCollider = Laser.AddComponent.<CapsuleCollider>();
+	var mesh : MeshRenderer = Laser.AddComponent.<MeshRenderer>();
+	var body : Rigidbody = Laser.AddComponent.<Rigidbody>();
+	var light : Light = Laser.AddComponent.<Light>();
+}
+
+function Update () {
+	if(Input.GetAxis("horizontal")){
+			Instantiate(Laser);
+			Laser.GetComponent
+
+	}
+}
