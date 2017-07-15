805837eebb0b71eda767295ec6eb6b672f7530b1
Daniel <daniel.bradley24@gmail.com>
2017-07-15 15:59:09 +0100

    Tech demoo

--- 6Shooter/Assets/EnemyController.js
+++ 6Shooter/Assets/EnemyController.js
@@ -6,12 +6,11 @@ var mesh : Mesh;
 
 function Start() {
 	health = 1;
-	currentPosition = transform.position;
 	mesh = Resources.Load("Assets/Enemy") as Mesh;
 }
 
 function Update() {
-	
+	currentPosition = transform.position;
 }
 
 function OnTriggerEnter(other : Collider) {
@@ -38,7 +37,7 @@ function TakeTurn() {
 			transform.position += Vector3.left;
 		}
 	}
-	if (Mathf.Abs(directionVector.z) >= Mathf.Abs(directionVector.z)) { //>= forces the enemy to default to moving in the x direction if the components of directionVector are the same sizeeeeeeeee
+	if (Mathf.Abs(directionVector.z) > Mathf.Abs(directionVector.x)) { //>= forces the enemy to default to moving in the x direction if the components of directionVector are the same sizeeeeeeeee
 		if (directionVector.z < 0) {
 			transform.position += Vector3.forward;
 		}
@@ -49,6 +48,6 @@ function TakeTurn() {
 }
 
 function DirectionVector(vector1 : Vector3,vector2 : Vector3) {
-	var directionVector : Vector3 = vector2 - vector1;
+	var directionVector : Vector3 = vector1 - vector2;
 	return(directionVector);
 }
--- 6Shooter/Assets/Movement.js
+++ 6Shooter/Assets/Movement.js
@@ -3,28 +3,27 @@ function Start() {
 }
 
 function Update() {
-	if (Input.GetKeyDown("left") === true) {
+	if (Input.GetKeyDown("left") && !Physics.Raycast(transform.position,Vector3.left,1.1)) {
 		transform.position += Vector3(-1,0,0);
 		gameObject.Find("Cube").GetComponent.<Rotation>().Rotate("right");
-		gameObject.GetComponent(Shoot).Shoot(4,1,0);
+		gameObject.GetComponent(Shoot).Shoot(4,50,0);
 		gameObject.Find("Enemy").GetComponent.<EnemyController>().TakeTurn();
 	}
-	if (Input.GetKeyDown("right") === true) {
+	if (Input.GetKeyDown("right") && !Physics.Raycast(transform.position,Vector3.right,1.1)) {
 		transform.position += Vector3(1,0,0);
 		gameObject.Find("Cube").GetComponent.<Rotation>().Rotate("left");
-		gameObject.GetComponent(Shoot).Shoot(2,-1,0);
+		gameObject.GetComponent(Shoot).Shoot(2,-50,0);
 		gameObject.Find("Enemy").GetComponent.<EnemyController>().TakeTurn();
 	}
-	if (Input.GetKeyDown("up") === true) {
+	if (Input.GetKeyDown("up") && !Physics.Raycast(transform.position,Vector3.forward,1.1)) {
 		transform.position += Vector3(0,0,1);
 		gameObject.Find("Cube").GetComponent.<Rotation>().Rotate("down");
-		gameObject.GetComponent(Shoot).Shoot(5,0,-1);
+		gameObject.GetComponent(Shoot).Shoot(5,0,-50);
 		gameObject.Find("Enemy").GetComponent.<EnemyController>().TakeTurn();
 	}
-	if (Input.GetKeyDown("down") === true) {
+	if (Input.GetKeyDown("down") && !Physics.Raycast(transform.position,Vector3.back,1.1)) {
 		transform.position += Vector3(0,0,-1);
 		gameObject.Find("Cube").GetComponent.<Rotation>().Rotate("up");
-		gameObject.GetComponent(Shoot).Shoot(1,0,1);
+		gameObject.GetComponent(Shoot).Shoot(1,0,50);
 		gameObject.Find("Enemy").GetComponent.<EnemyController>().TakeTurn();
-	}
-}
+	}}
