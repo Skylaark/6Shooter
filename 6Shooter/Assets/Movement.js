d520abb997d213ad23ec4e99abd39ba93d355657
Daniel <daniel.bradley24@gmail.com>
2017-07-15 12:18:57 +0100

    hAD TO HARD CODE INPUTS :((

--- 6Shooter/Assets/Movement.js
+++ 6Shooter/Assets/Movement.js
@@ -1,37 +1,32 @@
 #pragma strict
 
-var xMovement;
-var zMovement;
-
 function Start() {
 	
 }
 
 function Update() {
-	if (Vector3(Input.GetAxis("Horizontal"),0,0) == Vector3.right || Vector3(Input.GetAxis("Horizontal"),0,0) == Vector3.left) {
-		transform.position += Vector3(Input.GetAxis("Horizontal"),0,0);
-		if (Vector3(Input.GetAxis("Horizontal"),0,0) == Vector3.right && ) {
-			gameObject.Find("Cube").GetComponent.<Rotation>().Rotate("right");
-			gameObject.GetComponent(Shoot).Shoot(4,100,0);
-			//gameObject.Find("Enemy").GetComponent.EnemyController.TakeTurn();
-		}
-		if (Vector3(Input.GetAxis("Horizontal"),0,0) == Vector3.left) {
-			gameObject.Find("Cube").GetComponent.<Rotation>().Rotate("left");
-			gameObject.GetComponent(Shoot).Shoot(2,-100,0);
-			//gameObject.Find("Enemy").GetComponent.EnemyController.TakeTurn();
-		}
+	if (Input.GetKeyDown("left") === true) {
+		transform.position += Vector3(1,0,0);
+		gameObject.Find("Cube").GetComponent.<Rotation>().Rotate("right");
+		gameObject.GetComponent(Shoot).Shoot(4,100,0);
+		//gameObject.Find("Enemy").GetComponent.EnemyController.TakeTurn();
 	}
-	if (Vector3(0,0,Input.GetAxis("Vertical")) == Vector3.forward || Vector3(Input.GetAxis("Horizontal"),0,0) == Vector3.back) {
-		transform.position += Vector3(0,0,Input.GetAxis("Vertical"));
-		if (Vector3(0,0,Input.GetAxis("Vertical")) == Vector3.back) {
-			gameObject.Find("Cube").GetComponent.<Rotation>().Rotate("up");
-			gameObject.GetComponent(Shoot).Shoot(5,0,-100);
-			//gameObject.Find("Enemy").GetComponent.EnemyController.TakeTurn();
-		}
-		if (Vector3(0,0,Input.GetAxis("Vertical")) == Vector3.forward)	 {
-			gameObject.Find("Cube").GetComponent.<Rotation>().Rotate("down");
-			gameObject.GetComponent(Shoot).Shoot(1,0,100);
-			//gameObject.Find("Enemy").GetComponent.EnemyController.TakeTurn();
-		}
+	if (Input.GetKeyDown("right") === true) {
+		transform.position += Vector3(-1,0,0);
+		gameObject.Find("Cube").GetComponent.<Rotation>().Rotate("left");
+		gameObject.GetComponent(Shoot).Shoot(2,-100,0);
+		//gameObject.Find("Enemy").GetComponent.EnemyController.TakeTurn();
+	}
+	if (Input.GetKeyDown("up") === true) {
+		transform.position += Vector3(0,0,-1);
+		gameObject.Find("Cube").GetComponent.<Rotation>().Rotate("down");
+		gameObject.GetComponent(Shoot).Shoot(5,0,-100);
+		//gameObject.Find("Enemy").GetComponent.EnemyController.TakeTurn();
+	}
+	if (Input.GetKeyDown("down") === true) {
+		transform.position += Vector3(0,0,1);
+		gameObject.Find("Cube").GetComponent.<Rotation>().Rotate("up");
+		gameObject.GetComponent(Shoot).Shoot(1,0,100);
+		//gameObject.Find("Enemy").GetComponent.EnemyController.TakeTurn();
 	}
 }
