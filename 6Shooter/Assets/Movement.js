cf468fc5f0c6ba4e668e7623e85e2fc74c6cb169
Ali <escrime.ali@virginmedia.com>
2017-07-15 11:39:30 +0100

    Shooting works maybe

--- 6Shooter/Assets/Movement.js
+++ 6Shooter/Assets/Movement.js
@@ -12,12 +12,12 @@ function Update() {
 		transform.position += Vector3(Input.GetAxis("Horizontal"),0,0);
 		if (Vector3(Input.GetAxis("Horizontal"),0,0) == Vector3.right) {
 			gameObject.Find("Cube").GetComponent.Rotation.Rotate("right");
-			gameObject.GetComponent(Shoot).Shoot("left");
+			gameObject.GetComponent(Shoot).Shoot(4,100,0);
 			gameObject.Find("Enemy").GetComponent.EnemyController.TakeTurn();
 		}
 		if (Vector3(Input.GetAxis("Horizontal"),0,0) == Vector3.left) {
 			gameObject.Find("Cube").GetComponent.Rotation.Rotate("left");
-			gameObject.GetComponent(Shoot).Shoot("right");
+			gameObject.GetComponent(Shoot).Shoot(2,-100,0);
 			gameObject.Find("Enemy").GetComponent.EnemyController.TakeTurn();
 		}
 	}
@@ -25,12 +25,12 @@ function Update() {
 		transform.position += Vector3(0,0,Input.GetAxis("Vertical"));
 		if (Vector3(0,0,Input.GetAxis("Vertical")) == Vector3.back) {
 			gameObject.Find("Cube").GetComponent.Rotation.Rotate("up");
-			gameObject.GetComponent(Shoot).Shoot("down");
+			gameObject.GetComponent(Shoot).Shoot(5,0,-100);
 			gameObject.Find("Enemy").GetComponent.EnemyController.TakeTurn();
 		}
 		if (Vector3(0,0,Input.GetAxis("Vertical")) == Vector3.forward)	 {
 			gameObject.Find("Cube").GetComponent.Rotation.Rotate("down");
-			gameObject.GetComponent(Shoot).Shoot("up");
+			gameObject.GetComponent(Shoot).Shoot(1,0,100);
 			gameObject.Find("Enemy").GetComponent.EnemyController.TakeTurn();
 		}
 	}
