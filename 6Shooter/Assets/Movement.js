92a5b6fecb3542a08f9bc184d5e73a8a42e29ec0
Daniel <daniel.bradley24@gmail.com>
2017-07-15 09:51:12 +0100

    Updated Movement and Rotation Scripts

--- 6Shooter/Assets/Movement.js
+++ 6Shooter/Assets/Movement.js
@@ -1,20 +1,37 @@
 #pragma strict
-/*
+
 var xMovement;
-var yMovement;
+var zMovement;
 
 function Start() {
 	
 }
 
 function Update() {
-	if (Input.GetKeyDown("right" || "left") {
+	if (Vector3(Input.GetAxis("Horizontal"),0,0) == Vector3.right || Vector3.left) {
 		transform.position += Vector3(Input.GetAxis("Horizontal"),0,0);
-		if {Input.GetKeyDown("right") {
-			gameObject.Find(
+		if (Vector3(Input.GetAxis("Horizontal"),0,0) == Vector3.right) {
+			gameObject.Find("Cube").GetComponent.Animation.Rotate("right");
+			gameObject.GetComponent(Shoot).Shoot("left");
+			//Force enemies to take turn
+		}
+		if (Vector3(Input.GetAxis("Horizontal"),0,0) == Vector3.left) {
+			gameObject.Find("Cube").GetComponent.Animation.Rotate("left");
+			gameObject.GetComponent(Shoot).Shoot("right");
+			// Force enemies to take turn
+		}
 	}
-	if (Input.GetKeyDown("up" || "down") {
+	if (Vector3(0,0,Input.GetAxis("Vertical")) == Vector3.forward || Vector3.back) {
 		transform.position += Vector3(0,0,Input.GetAxis("Vertical"));
+		if (Vector3(0,0,Input.GetAxis("Vertical")) == Vector3.back) {
+			gameObject.Find("Cube").GetComponent.Animation.Rotate("up");
+			gameObject.GetComponent(Shoot).Shoot("down");
+			//Force enemies to take turn
+		}
+		if (Vector3(0,0,Input.GetAxis("Vertical")) == Vector3.forward)	 {
+			gameObject.Find("Cube").GetComponent.Animation.Rotate("down");
+			gameObject.GetComponent(Shoot).Shoot("up");
+			//Force enemies to take turn
+		}
 	}
 }
-*/
--- 6Shooter/Assets/Rotation.js
+++ 6Shooter/Assets/Rotation.js
@@ -1,32 +1,48 @@
 #pragma strict
 
-var rotateUp = new Array [[0,0,0,0,0,1],[0,1,0,0,0,0],[1,0,0,0,0,0],[0,0,0,1,0,0],[0,0,1,0,0,0],[0,0,0,0,1,0]];
-var rotateDown = new Array [[0,0,1,0,0,0],[0,1,0,0,0,0],[0,0,0,0,1,0],[0,0,0,1,0,0],[0,0,1,0,0,0],[1,0,0,0,0,0]];
-var rotateRight = new Array [[1,0,0,0,0,0],[0,0,1,0,0,0],[0,0,0,1,0,0],[0,0,0,0,0,1],[0,0,0,0,1,0],[0,1,0,0,0,0]];
-var rotateLeft = new Array [[1,0,0,0,0,0],[0,0,0,0,0,1],[0,1,0,0,0,0],[0,0,1,0,0,0],[0,0,0,0,1,0],[0,0,0,1,0,0]];
+var rotateUp = new Array [Array[0,0,0,0,0,1],Array[0,1,0,0,0,0],Array[1,0,0,0,0,0],Array[0,0,0,1,0,0],Array[0,0,1,0,0,0],Array[0,0,0,0,1,0]];
+var rotateDown = new Array [Array[0,0,1,0,0,0],Array[0,1,0,0,0,0],Array[0,0,0,0,1,0],Array[0,0,0,1,0,0],Array[0,0,1,0,0,0],Array[1,0,0,0,0,0]];
+var rotateRight = new Array [Array[1,0,0,0,0,0],Array[0,0,1,0,0,0],Array[0,0,0,1,0,0],Array[0,0,0,0,0,1],Array[0,0,0,0,1,0],Array[0,1,0,0,0,0]];
+var rotateLeft = new Array [Array[1,0,0,0,0,0],Array[0,0,0,0,0,1],Array[0,1,0,0,0,0],Array[0,0,1,0,0,0],Array[0,0,0,0,1,0],Array[0,0,0,1,0,0]];
 
-var currentPosition;
+var currentRotation;
+var rotToPerform;
 
 function Start() {
-	var currentPosition = transform.position;
+	currentRotation = new Array [1,2,3,4,5,6];
 }
 
 function Update() {
 	
 }
 
-function RotateUp() {
-	
+function Rotate(direction) {
+	switch(direction) {
+		case "right":
+			rotToPerform = rotateRight;
+			VectorMultiplaction();
+			transform.rotation += Quaternion.Euler(90,0,0)
+		case "left":
+			rotToPerform = rotateLeft;
+			VectorMultiplaction();
+			transform.rotation += Quaternion.Euler(-90,0,0)
+		case "up":
+			rotToPerform = rotateUp;
+			VectorMultiplaction();
+			transform.rotation += Quaternion.Euler(0,0,90)
+		case "down":
+			rotToPerform = rotateDown;
+			VectorMultiplaction();
+			transform.rotation += Quaternion.Euler(0,0,-90)
+	}
 }
 
-function RotateRight() {
-	
-}
-
-function RotateLeft() {
-	
-}
-
-function RotateDown() {
-	
+function VectorMultiplaction() {
+	var newRotation
+	for (i : int in range(0,5)) {
+		for (j : int in range(0,5)) {
+			newRotation[i] += new Array[currentRotation[j]*rotToPerform[i][j]]
+		}
+	}
+	currentRotation = newRotation
 }
