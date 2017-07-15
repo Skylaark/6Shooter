01d136a279c1b606ec948e2411783560d35d99d2
Ali <escrime.ali@virginmedia.com>
2017-07-15 12:20:13 +0100

    Materials made

--- 6Shooter/Assets/Shoot.js
+++ 6Shooter/Assets/Shoot.js
@@ -12,6 +12,7 @@ function Update () {
 
 function Shoot(i,x,z){
 	var Laser = new GameObject("Laser",CapsuleCollider,MeshFilter,MeshRenderer,Rigidbody,Light);
+	var fileEnd : String = i as String;
 	Laser.GetComponent.<CapsuleCollider>().height = 1.5;
 	Laser.GetComponent.<CapsuleCollider>().radius = 0.05;
 	Laser.GetComponent.<CapsuleCollider>().direction = 1;
@@ -21,26 +22,6 @@ function Shoot(i,x,z){
 	Laser.GetComponent.<Light>().intensity = 50;
 	Laser.GetComponent.<Light>().range = 50;
 	Laser.GetComponent.<MeshFilter>().mesh = mesh;
-	switch(GameObject.Find("Cube").GetComponent.<Rotation>().currentRotation[i]){
-		case(1):
-			Laser.GetComponent.<MeshRenderer>().material = Resources.Load("Materials/Laser Red") as Material;
-			break;
-		case(2):
-			Laser.GetComponent.<MeshRenderer>().material = Resources.Load("Materials/Laser Blue") as Material;
-			break;
-		case(3):
-			Laser.GetComponent.<MeshRenderer>().material = Resources.Load("Materials/Laser Green") as Material;
-			break;
-		case(4):
-			Laser.GetComponent.<MeshRenderer>().material = Resources.Load("Materials/Laser Yellow") as Material;
-			break;
-		case(5):
-			Laser.GetComponent.<MeshRenderer>().material = Resources.Load("Materials/Laser Orange") as Material;
-			break;
-		case(6):
-			Laser.GetComponent.<MeshRenderer>().material = Resources.Load("Materials/Laser Purple") as Material;
-			break;
-		default:break;
-	}
+	Laser.GetComponent.<MeshRenderer>().material = Resources.Load("Assets/Materials/Laser"+fileEnd) as Material;
 	Laser.GetComponent.<Rigidbody>().velocity = Vector3(x,0,z);
 }
