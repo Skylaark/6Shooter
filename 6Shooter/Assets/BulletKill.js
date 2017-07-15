c2e2e9491fb49b45d014aac291490cd6097ee6f3
Ali <escrime.ali@virginmedia.com>
2017-07-15 15:57:57 +0100

    Bullets kill themselves and have different colours

---
+++ 6Shooter/Assets/BulletKill.js
@@ -0,0 +1,3 @@
+#pragma strictvar insidePlayer : System.Boolean = true;
+function Start() {
+}function OnTriggerEnter(other : Collider){	if(!insidePlayer){		Destroy(this.gameObject);	}}function OnTriggerExit(other : Collider){	insidePlayer = false;}
--- 6Shooter/Assets/Shoot.js
+++ 6Shooter/Assets/Shoot.js
@@ -1,6 +1,8 @@
 ﻿#pragma strict
 
 var mesh : Mesh;
+var colours : Array = new Array(Vector4(1,0,0,1),Vector4(0,1,0,1),Vector4(0,0,1,1),Vector4(1,1,0,1),Vector4(1,0,1,1),Vector4(0,1,1,1));
+
 
 function Start () {
 	mesh = Resources.Load("Models/bullet",typeof(Mesh));
@@ -10,19 +12,21 @@ function Update () {
 
 }
 
-function Shoot(i,x,z){
+function Shoot(i : int,x,z){
 	var Laser = new GameObject("Laser",CapsuleCollider,MeshFilter,MeshRenderer,Rigidbody,Light);
+	var colour : Vector4 = colours[i-1];
 	Laser.GetComponent.<CapsuleCollider>().height = 1.5;
 	Laser.GetComponent.<CapsuleCollider>().radius = 0.05;
 	Laser.GetComponent.<CapsuleCollider>().direction = 1;
 	Laser.GetComponent.<CapsuleCollider>().isTrigger = true;
 	Laser.transform.position = GameObject.Find("Player").transform.position;
 	Laser.GetComponent.<Rigidbody>().useGravity = false;
-	Laser.GetComponent.<Light>().color = Vector4(1,0,0,1);
+	Laser.GetComponent.<Light>().color = colour;
 	Laser.GetComponent.<Light>().intensity = 1;
 	Laser.GetComponent.<Light>().range = 25;
 	Laser.GetComponent.<MeshFilter>().mesh = mesh;
 	Laser.GetComponent.<MeshRenderer>().material = Resources.Load("Materials/Laser"+i.ToString());
-	Debug.Log("Materials/Laser"+i.ToString());
 	Laser.GetComponent.<Rigidbody>().velocity = Vector3(x,0,z);
+	Laser.AddComponent(BulletKill);
 }
+
