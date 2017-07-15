7ad0d52f7d7db08c9764391f4946e88e86a239b2
Ali <escrime.ali@virginmedia.com>
2017-07-15 10:48:32 +0100

    shoot n stuff

--- 6Shooter/Assets/Rotation.js
+++ 6Shooter/Assets/Rotation.js
@@ -1,53 +1,53 @@
-#pragma strict
-
-var rotateUp : Array = new Array([0,0,0,0,0,1],[0,1,0,0,0,0],[1,0,0,0,0,0],[0,0,0,1,0,0],[0,0,1,0,0,0],[0,0,0,0,1,0]);
-var rotateDown : Array = new Array([0,0,1,0,0,0],[0,1,0,0,0,0],[0,0,0,0,1,0],[0,0,0,1,0,0],[0,0,1,0,0,0],[1,0,0,0,0,0]);
-var rotateRight : Array = new Array([1,0,0,0,0,0],[0,0,1,0,0,0],[0,0,0,1,0,0],[0,0,0,0,0,1],[0,0,0,0,1,0],[0,1,0,0,0,0]);
-var rotateLeft : Array = new Array([1,0,0,0,0,0],[0,0,0,0,0,1],[0,1,0,0,0,0],[0,0,1,0,0,0],[0,0,0,0,1,0],[0,0,0,1,0,0]);
-
-var currentRotation : Array;
-var rotToPerform : Array;
-
-function Start() {
-	currentRotation = new Array(1,2,3,4,5,6);
-}
-
-function Update() {
-	
-}
-
-function Rotate(direction) {
-	switch(direction) {
-		case "right":
-			rotToPerform = rotateRight;
-			VectorMultiplaction();
-			transform.Rotate(90,0,0);
-		case "left":
-			rotToPerform = rotateLeft;
-			VectorMultiplaction();
-			transform.Rotate(-90,0,0);
-		case "up":
-			rotToPerform = rotateUp;
-			VectorMultiplaction();
-			transform.Rotate(0,0,90);
-		case "down":
-			rotToPerform = rotateDown;
-			VectorMultiplaction();
-			transform.Rotate(0,0,-90);
-	}
-}
-
-function VectorMultiplaction() {
-	var newRotation : Array = [0,0,0,0,0,0];
-	var c : int; 
-	for (var i : int in range(0,5)) {
-		for (var j : int in range(0,5)) {
-			var row : Array = rotToPerform[i];
-			var a : int = currentRotation[j];
-			var b : int = row[j];
-			c += a*b;
-			newRotation[i] = c;
-		}
-	}
-	currentRotation = newRotation;
+#pragma strict
+
+var rotateUp : Array = new Array([0,0,0,0,0,1],[0,1,0,0,0,0],[1,0,0,0,0,0],[0,0,0,1,0,0],[0,0,1,0,0,0],[0,0,0,0,1,0]);
+var rotateDown : Array = new Array([0,0,1,0,0,0],[0,1,0,0,0,0],[0,0,0,0,1,0],[0,0,0,1,0,0],[0,0,1,0,0,0],[1,0,0,0,0,0]);
+var rotateRight : Array = new Array([1,0,0,0,0,0],[0,0,1,0,0,0],[0,0,0,1,0,0],[0,0,0,0,0,1],[0,0,0,0,1,0],[0,1,0,0,0,0]);
+var rotateLeft : Array = new Array([1,0,0,0,0,0],[0,0,0,0,0,1],[0,1,0,0,0,0],[0,0,1,0,0,0],[0,0,0,0,1,0],[0,0,0,1,0,0]);
+
+var currentRotation : Array;
+var rotToPerform : Array;
+
+function Start() {
+	currentRotation = new Array(1,2,3,4,5,6);
+}
+
+function Update() {
+	
+}
+
+function Rotate(direction) {
+	switch(direction) {
+		case "right":
+			rotToPerform = rotateRight;
+			VectorMultiplaction();
+			transform.Rotate(90,0,0);
+		case "left":
+			rotToPerform = rotateLeft;
+			VectorMultiplaction();
+			transform.Rotate(-90,0,0);
+		case "up":
+			rotToPerform = rotateUp;
+			VectorMultiplaction();
+			transform.Rotate(0,0,90);
+		case "down":
+			rotToPerform = rotateDown;
+			VectorMultiplaction();
+			transform.Rotate(0,0,-90);
+	}
+}
+
+function VectorMultiplaction() {
+	var newRotation : Array = [0,0,0,0,0,0];
+	var c : int; 
+	for (var i : int in range(0,5)) {
+		for (var j : int in range(0,5)) {
+			var row : Array = rotToPerform[i];
+			var a : int = currentRotation[j];
+			var b : int = row[j];
+			c += a*b;
+			newRotation[i] = c;
+		}
+	}
+	currentRotation = newRotation;
 }
--- 6Shooter/Assets/Shoot.js
+++ 6Shooter/Assets/Shoot.js
@@ -1,21 +1,25 @@
 ﻿#pragma strict
 
+var mesh : Mesh;
+
 function Start () {
-//	var mesh : Mesh = 
+	mesh = Resources.Load("Assets/Bullet") as Mesh;
 }
 
 function Update () {
-	if(Input.GetKeyDown("d")){
+	if(Vector3(Input.GetAxis("Horizontal"),0,0) == Vector3.right || Vector3.left){
 		var Laser = new GameObject("Laser",CapsuleCollider,MeshFilter,MeshRenderer,Rigidbody,Light);
 		Laser.GetComponent.<CapsuleCollider>().height = 1.5;
 		Laser.GetComponent.<CapsuleCollider>().radius = 0.05;
 		Laser.GetComponent.<CapsuleCollider>().direction = 1;
 		Laser.transform.position = GameObject.Find("Player").transform.position;
-		Laser.GetComponent.<Rigidbody>().velocity = Vector3(100,0,0);
 		Laser.GetComponent.<Rigidbody>().useGravity = false;
 		Laser.GetComponent.<Light>().color = Vector4(1,0,0,1);
-		Laser.GetComponent.<Light>().intensity = 100;
-		Laser.GetComponent.<Light>().range = 100;
-		Laser.GetComponent.<MeshFilter>().mesh = Capsule;
+		Laser.GetComponent.<Light>().intensity = 50;
+		Laser.GetComponent.<Light>().range = 50;
+		Laser.GetComponent.<MeshFilter>().mesh = mesh;
+		Laser.GetComponent.<MeshRenderer>().material = Resources.Load("Assets/Laser Material") as Material;
+		Laser.GetComponent.<Rigidbody>().velocity = Vector3(100*Input.GetAxis("Horizontal"),0,0);
+
 	}
 }
